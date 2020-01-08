40133db0546ea0e3daf441a78a50121e276cdccd
Chris Carr <christocarr@outlook.com>
2020-01-08 14:26:27 +0000

    Add user route

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const database = {
   users: [
@@ -13,47 +13,60 @@ const database = {
       email: 'john@gmail.com',
       password: 'password',
       entries: 3,
-      joined: new Date()
+      joined: new Date(),
     },
     {
       id: 124,
       name: 'Sally',
       email: 'sally@gmail.com',
       password: 'bananas',
-      joined: new Date()
-    }
-  ]
-}
+      joined: new Date(),
+    },
+  ],
+};
 
 app.get('/', (req, res) => {
-  res.send('working')
-})
+  res.send(database.users);
+});
 
 //signin route
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email && 
-      req.body.password === database.users[0].password) {
-    res.json('success')
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json('success');
   } else {
-    res.status(400).json('error logging in')
+    res.status(400).json('error logging in');
   }
-})
+});
 
 //register route
 app.post('/register', (req, res) => {
-  
-  const {name, email, password} = req.body
+  const { name, email, password } = req.body;
   database.users.push({
     id: 125,
     name: name,
     email: email,
     password: password,
     entries: 0,
-    joined: new Date()
-  })
+    joined: new Date(),
+  });
   res.json(database.users[database.users.length - 1]);
-})
+});
+
+//user route
+app.get('profile/:id', (req, res) => {
+  const { id } = req.params;
+  database.users.forEach(user => {
+    if (user.id === id) {
+      res.json(user);
+    } else {
+      res.status(404).json('No such user');
+    }
+  });
+});
 
 app.listen(3000, () => {
-  console.log('listening')
-})
+  console.log('listening');
+});
