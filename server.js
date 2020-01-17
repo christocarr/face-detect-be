de196780fd2bdd970b3e5c6e9644355a6e92727d
Chris Carr <christocarr@outlook.com>
2020-01-17 18:25:50 +0000

    Add knex transaction to add hash password to postgres table

--- server.js
+++ server.js
@@ -38,15 +38,26 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
   const { name, email, password } = req.body;
   const hash = bcrypt.hashSync(password)
-
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+        .then(user => res.json(user[0]))
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
     })
-    .then(user => res.json(user[0]))
     .catch(err => res.status(400).json('Unable to register'));
 });
 
