3dd631074507b597df69c95afe80faccb82bf9c8
Chris Carr <christocarr@outlook.com>
2020-01-17 18:49:06 +0000

    Add condition if password is incorrect

--- server.js
+++ server.js
@@ -30,13 +30,16 @@ app.post('/signin', (req, res) => {
     .then(data => {
       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       if (isValid) {
-        return db.select('*')
+        return db
+          .select('*')
           .from('users')
           .where('email', '=', req.body.email)
           .then(user => {
             res.json(user[0]);
           })
           .catch(err => res.status(400).json('unable to get user'));
+      } else {
+        res.status(400).json('wrong credentials');
       }
     })
     .catch(err => res.status(400).json('wrong credentials'));
